705115976d46144ef4061f587cf2b5ce341045a9
Yohei Okaya <okaya.dcinc@gmail.com>
2021-07-15 14:51:13 +0900

    fix cloud

--- cloud/src/utils/mqtt.ts
+++ cloud/src/utils/mqtt.ts
@@ -1,13 +1,20 @@
 import MQTT from 'async-mqtt'
+import fs from 'fs';
 
 let client: MQTT.AsyncMqttClient
 
 export const connect = () => {
+  // client = MQTT.connect(process.env.BROKER_URL, {
+  //   username: process.env.BROKER_USER,
+  //   password: process.env.BROKER_PASSWORD
+  // })
   client = MQTT.connect(process.env.BROKER_URL, {
-    username: process.env.BROKER_USER,
-    password: process.env.BROKER_PASSWORD
+    key: fs.readFileSync('key'),
+    cert: fs.readFileSync('cert'),
+    ca: fs.readFileSync('ca')
   })
   return client
+
 }
 
 export { client }
